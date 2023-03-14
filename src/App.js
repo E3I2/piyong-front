232c12c0501747b7d99aefdb5e20faeffb1c9bef
eunne2 <ehye518@gmail.com>
2023-03-14 16:18:25 +0900

    헤더 햄버거 스타일 수정 / 관리자 계정 헤더 루트 및 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Footer from "./components/common/footer/Footer";
 import Header from "./components/common/header/Header";
 import Link from "./pages/home/Link";
+import HeaderAdmin from "./components/common/header/HeaderAdmin";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
---
+++ src/components/common/header/HeaderAdmin.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useRef } from "react";
+import styles from "./HeaderAdmin.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import logoPath from "./img/ppiyong.png";
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toogleMenu = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+  const menuRef = useRef();
+  useEffect(() => {
+    menuRef.current.classList.add("active");
+  }, []);
+
+  return (
+    <nav className={styles.nav} id="nav">
+      <NavLink to="/" style={{ textDecoration: "none", color: "#000" }}>
+        <div className={styles.logoBox} id="logoBox">
+          <img src={logoPath} alt="logo" className={styles.logoImg} />
+          <div className={styles.titleBox}>
+            <p className={styles.logoTitle}>PIYONG</p>
+            <p className={styles.logoInfo}>둔산경찰서</p>
+          </div>
+        </div>
+      </NavLink>
+      <ul
+        className={isOpen ? styles.active : styles.links}
+        id="links"
+        ref={menuRef}
+        style={{ padding: "0" }}
+      >
+        <NavLink
+          to="./recode"
+          style={{ textDecoration: "none", color: "#000" }}
+        >
+          <li className={styles.link}>순찰일지</li>
+        </NavLink>
+        <NavLink
+          to="./request"
+          style={{ textDecoration: "none", color: "#000" }}
+        >
+          <li className={styles.link}>순찰요청</li>
+        </NavLink>
+        <NavLink
+          to="./community"
+          style={{ textDecoration: "none", color: "#000" }}
+        >
+          <li className={styles.link}>커뮤니티</li>
+        </NavLink>
+        <NavLink
+          to="./monitoring"
+          style={{ textDecoration: "none", color: "#000" }}
+        >
+          <li className={styles.link}>순찰하기</li>
+        </NavLink>
+        <NavLink to="./admin" style={{ textDecoration: "none", color: "#000" }}>
+          <li className={styles.link}>관리</li>
+        </NavLink>
+      </ul>
+      <button
+        onClick={() => toogleMenu()}
+        className={styles.toogleBtn}
+        id="toogleBtn"
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+    </nav>
+  );
+}
+
+export default Header;
