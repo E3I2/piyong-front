94088cbecbc2e92d274a6ec21e8d0b101605e316
SeonWoong Yang <byuljinarin@gmail.com>
2023-03-16 09:26:46 +0900

    03/16 09:26 Admin 수정 내용 커밋

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,29 +1,8 @@
 import React from 'react';
 import styles from './Admin.module.css';
 import { useState } from "react";
+import Board from '../AdminTable/Board';
 
-function Toggle({ title, content }) {
-  
-    const [isCheck, setCheck] = useState(false);
-  
-    return (
-      <>
-        <div
-        >
-          <button
-            onClick={() => {
-              setCheck((e) => !e);
-            }}
-          >
-            {isCheck ? "회원관리페이지" : "작성페이지"}
-          </button>
-        </div>
-        {isCheck && (
-          1234
-        )}
-      </>
-    );
-  }
   
 
 function Admin(){
@@ -31,14 +10,11 @@ function Admin(){
     return <div>
         <div className={styles.LineSection}>
             <div className={styles.PatrolSection}>
-                <div className={styles.TitleSection}><Toggle></Toggle></div>
+                <div className={styles.TitleSection}></div>
             <table className={styles.Table}>      
                 <thead>
                     <tr>
-                        <th className={styles.TableTitleSection}>관리자 추가</th>
-                        <th className={styles.TableTitleSection}>회원 관리자 지정</th>
-                        <th className={styles.TableTitleSection}>일반 회원 지정</th>
-                        <th className={styles.TableTitleSection}>회원 목록 조회</th>
+                        <th><Board></Board></th>
                     </tr>
                 </thead>
             </table>
