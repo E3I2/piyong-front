0072621d4b21569f6ee5136c8715bcf141c5b48a
eunne2 <ehye518@gmail.com>
2023-03-21 17:18:10 +0900

    CommunityDetails 디자인 완료 (데이터 연동 및 기능 추가 필요)

--- src/pages/community/CommunityDetails.js
+++ src/pages/community/CommunityDetails.js
@@ -27,15 +27,103 @@ function CommunityDetails() {
 
   return (
     <>
-      <Category />
+      <Category category={"커뮤니티 - 동네 정보 공유"} text={"커뮤니티"} />
       <MainBox>
         <PostsBox>
-          <div>
+          {/* <div>
             <div>{list.title}</div>
             <div>{list.content}</div>
+          </div> */}
+          <div className={styles.subject}>
+            <div className={styles.title}>제목</div>
+            <div className={styles.postInfo}>
+              <div className={styles.userBox}>
+                <div className={styles.img}></div>
+                <div className={styles.userInfo}>
+                  <div className={styles.userId}>아이디</div>
+                  <div className={styles.created}>2023.03.22. 10:03</div>
+                </div>
+              </div>
+              <div className={styles.InfoBox}>
+                <div className={styles.likes}>
+                  <div>♡</div>
+                  <div>좋아요</div>
+                  <div>0</div>
+                </div>
+                <div className={styles.comment}>
+                  <div>○</div>
+                  <div>댓글</div>
+                  <div>1</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Hr />
+          <div className={styles.content}>본문</div>
+          <Hr />
+
+          {/* 댓글 */}
+          <div className={styles.commentBox}>
+            <div className={styles.commentTitle}>댓글</div>
+            <div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "flex-start",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div className={styles.commentUser}>
+                  <div className={styles.img}></div>
+                  <div className={styles.userId} style={{ margin: "0 10px" }}>
+                    아이디
+                  </div>
+                  <div className={styles.created}>2023.03.22. 13:28</div>
+                </div>
+                <div>
+                  <button className={styles.commentBtn}>수정</button>
+                  <button className={styles.commentBtn}>삭제</button>
+                </div>
+              </div>
+              <div className={styles.commentContent}>댓글내용</div>
+              <Hr />
+              <div className={styles.commentPostBox}>
+                <textarea
+                  placeholder="댓글을 입력해 주세요."
+                  className={styles.commentPost}
+                ></textarea>
+                <button className={styles.commentPostBtn}>등록</button>
+              </div>
+            </div>
           </div>
         </PostsBox>
       </MainBox>
+
+      {/* 버튼 */}
+      <div className={styles.btnBox}>
+        <span className={styles.btn}>
+          <NavLink
+            className={({ isActive }) =>
+              "nav-link" + (isActive ? " click" : "")
+            }
+            to="/community"
+            style={{ textDecoration: "none" }}
+          >
+            <Button selectBtn={6} text={"목록"} />
+          </NavLink>
+        </span>
+        <span className={styles.btn2}>
+          <NavLink
+            className={({ isActive }) =>
+              "nav-link" + (isActive ? " click" : "")
+            }
+            to="/community-write"
+            style={{ textDecoration: "none" }}
+          >
+            <Button selectBtn={1} text={"작성하기"} />
+          </NavLink>
+        </span>
+      </div>
     </>
   );
 }
@@ -55,11 +143,10 @@ const PostsBox = styled.div`
   margin: auto;
   width: calc(100% - 3%);
   min-height: 250px;
-  box-shadow: 1px 1px 40px #909090;
+  box-shadow: 1px 1px 15px #909090;
   letter-spacing: -0.5px;
-  border: 1px solid rgba(0, 0, 0, 0.6);
+  border: 1px solid rgba(0, 0, 0, 0.3);
   border-radius: 3px;
-  padding: 30px 0;
 `;
 
 const MainBox = styled.div`
